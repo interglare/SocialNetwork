f157e91648c7eaaa71381fda20518f2e77c7cf9d
interglare <47270149+interglare@users.noreply.github.com>
2021-05-12 20:16:36 +0600

    added Stack navigator with 2 screens and context

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//Zhanibek Orazbai 20MD0214
+import "react-native-gesture-handler";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { FriendsContext } from "./FriendsContext";
+import HomeScreen from "./HomeScreen";
+import FriendsScreen from "./FriendsScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      possibleFriends: ["Alice", "Bob", "Sammy"],
+      currentFriends: [],
+    };
+  }
+
+  addFriend = (index) => {
+    const { currentFriends, possibleFriends } = this.state;
+
+    // Pull friend out of possibleFriends
+    const addedFriend = possibleFriends.splice(index, 1);
+
+    // And put friend in currentFriends
+    currentFriends.push(addedFriend);
+
+    // Finally, update the app state
+    this.setState({
+      currentFriends,
+      possibleFriends,
+    });
+  };
+
+  render() {
+    return (
+      <FriendsContext.Provider
+        value={
+          {
+            currentFriends: this.state.currentFriends,
+            possibleFriends: this.state.possibleFriends,
+            addFriend: this.addFriend
+          }
+        }
+      >
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Friends" component={FriendsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FriendsContext.Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
